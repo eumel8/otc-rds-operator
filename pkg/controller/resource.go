4be90bc8c8032e975e7e804e4904c0fd5024aca1
Frank Kloeker <f.kloeker@telekom.de>
2022-04-20 19:29:38 +0200

    try again

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -280,8 +280,8 @@ func rdsUpdate(ctx context.Context, client *golangsdk.ServiceClient, oldRds *rds
 	}
 	if newRds.Spec.Reboot == true {
 		fmt.Println("doing restart")
-		restartOpts := instances.RestartRdsInstanceOpts{Restart: "{}"}
-		restartResult := instances.Restart(client, restartOpts, newRds.Status.Id)
+		// restartOpts := instances.RestartRdsInstanceOpts{Restart: "{}"}
+		restartResult := instances.Restart(client, instances.RestartRdsInstanceOpts{Restart: "{}"}, newRds.Status.Id)
 		_, err := restartResult.Extract()
 		if err != nil {
 			err := fmt.Errorf("error resizing rds: %v", err)
