54336fd247728fba6f05cf9fda77f67c0913eb56
Frank Kloeker <f.kloeker@telekom.de>
2022-04-21 17:57:14 +0200

    reboot finished, re-act errorlogs

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -317,29 +317,28 @@ func rdsUpdate(ctx context.Context, client *golangsdk.ServiceClient, oldRds *rds
 			return err
 		}
 	}
-	/*
-		fmt.Println("doing errorlog catchup")
-		errorLogOpts := instances.DbErrorlogOpts{StartDate: "2021-04-01T00:00:00+0000", EndDate: "2022-04-21T00:00:00+0000", Level: "ALL"}
-		allPages, err := instances.ListErrorLog(client, errorLogOpts, newRds.Status.Id).AllPages()
-		if err != nil {
-			err := fmt.Errorf("error getting rds pages: %v", err)
-			return err
-		}
-		errorLogs, err := instances.ExtractErrorLog(allPages)
-		if err != nil {
-			err := fmt.Errorf("error getting rds errorlog: %v", err)
-			return err
-		}
 
-		// for _, li := range errorLogs {
-		fmt.Println(errorLogs)
-		// 	}
-		// newRds.Status.Events = errorLogs.ErrorLogList.ErrorLog.Content
-		if err := UpdateStatus(ctx, newRds, namespace); err != nil {
-			err := fmt.Errorf("error update rds error log events: %v", err)
-			return err
-		}
-	*/
+	fmt.Println("doing errorlog catchup")
+	errorLogOpts := instances.DbErrorlogOpts{StartDate: "2021-04-01T00:00:00+0000", EndDate: "2022-04-21T00:00:00+0000", Level: "ALL"}
+	allPages, err := instances.ListErrorLog(client, errorLogOpts, newRds.Status.Id).AllPages()
+	if err != nil {
+		err := fmt.Errorf("error getting rds pages: %v", err)
+		return err
+	}
+	errorLogs, err := instances.ExtractErrorLog(allPages)
+	if err != nil {
+		err := fmt.Errorf("error getting rds errorlog: %v", err)
+		return err
+	}
+
+	// for _, li := range errorLogs {
+	fmt.Println(errorLogs)
+	// 	}
+	// newRds.Status.Events = errorLogs.ErrorLogList.ErrorLog.Content
+	if err := UpdateStatus(ctx, newRds, namespace); err != nil {
+		err := fmt.Errorf("error update rds error log events: %v", err)
+		return err
+	}
 	/* What we have todo here:
 	* Error Logs https://github.com/opentelekomcloud/gophertelekomcloud/blob/devel/openstack/rds/v3/instances/requests.go#L302
 	* Slow Logs https://github.com/opentelekomcloud/gophertelekomcloud/blob/devel/openstack/rds/v3/instances/requests.go#L375
