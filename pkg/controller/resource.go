8c6455f0d815cc73c64f2bb39328542ac8dabf71
Frank Kloeker <f.kloeker@telekom.de>
2022-04-21 14:08:47 +0200

    fix time set

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -309,7 +309,7 @@ func rdsUpdate(ctx context.Context, client *golangsdk.ServiceClient, oldRds *rds
 		}
 	}
 	fmt.Println("doing errorlog catchup")
-	errorLogOpts := instances.DbErrorlogOpts{StartDate: "1970-01-01T00:00:00+00:00", EndDate: "2099-01-01T00:00:00+00:00", Level: "ALL"}
+	errorLogOpts := instances.DbErrorlogOpts{StartDate: "1970-01-01T00:00:00+0000", EndDate: "2099-01-01T00:00:00+0000", Level: "ALL"}
 	allPages, err := instances.ListErrorLog(client, errorLogOpts, newRds.Status.Id).AllPages()
 	if err != nil {
 		err := fmt.Errorf("error getting rds pages: %v", err)
