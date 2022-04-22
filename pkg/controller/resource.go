902f106a9190c60889305fce0b2186e67ce9eca5
Frank Kloeker <eumel@arcor.de>
2022-04-22 23:11:32 +0200

    check privateip

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -502,6 +502,8 @@ func rdsUpdateStatus(ctx context.Context, client *golangsdk.ServiceClient, newRd
 	rdsInstance, err := rdsGetByName(client, newRds.Name)
 	if len(rdsInstance.PrivateIps) > 0 {
 		newRds.Status.Ip = rdsInstance.PrivateIps[0]
+		fmt.Println("doing privip")
+		fmt.Println(newRds.Status.Ip)
 	} else {
 		newRds.Status.Ip = ""
 	}
