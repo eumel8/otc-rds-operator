cc0a651c62f1b7f858dd9b41a6769a298aa61dc1
Frank Kloeker <eumel@arcor.de>
2022-05-01 23:19:59 +0200

    fix error handling sql

--- pkg/controller/sql.go
+++ pkg/controller/sql.go
@@ -37,30 +37,38 @@ func createSqlUser(newRds *rdsv1alpha1.Rds) error {
 			if res != nil {
 				fmt.Println("grant access here and create user")
 				_, err := db.Query("CREATE USER '" + su.Name + "'@'" + su.Host + "' IDENTIFIED BY '" + su.Password + "'")
-				fmt.Printf("error creating user: %v", err)
+				fmt.Printf("error creating user: %v\n", err)
 
 				for _, pr := range su.Privileges {
 					fmt.Println("PRIV")
 					// this query must be validated against sql injection
-					_, err := db.Query(pr + ";FLUSH PRIVILEGES")
-					fmt.Printf("error creating privileges: %v", err)
+					_, err := db.Query(pr)
+					if err != nil {
+						fmt.Printf("error creating privileges: %v\n", err)
+					}
+					_, err = db.Query("FLUSH PRIVILEGES")
+
+					if err != nil {
+						fmt.Printf("error flush privileges: %v\n", err)
+					}
+
 				}
 			}
 		}
 
 		for _, ds := range *&newRds.Spec.Databases {
 			res, err := db.Query("SELECT schema_name FROM information_schema.schemata WHERE schema_name='" + ds + "'")
-
 			if err != nil {
-				err := fmt.Errorf("error query user: %v", err)
+				err := fmt.Errorf("error query user: %v\n", err)
 				return err
 			}
 
 			if res != nil {
 				fmt.Println("create database")
 				_, err := db.Query("CREATE DATABASE '" + ds + "")
-				fmt.Printf("error creating database: %v", err)
-
+				if err != nil {
+					fmt.Printf("error creating database: %v\n", err)
+				}
 			}
 		}
 
