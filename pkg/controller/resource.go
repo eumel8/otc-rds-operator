8158dfb469bb18a15c4f9c7af265696936007913
Frank Kloeker <eumel@arcor.de>
2022-05-01 20:14:43 +0200

    error return

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -286,7 +286,11 @@ func (c *Controller) rdsUpdate(ctx context.Context, client *golangsdk.ServiceCli
 		return err
 	}
 	// Enlarge volume here
-	createSqlUser(newRds)
+	err := createSqlUser(newRds)
+	if err != nil {
+		err := fmt.Errorf("error createSqlUser: %v", err)
+		return err
+	}
 	if oldRds.Spec.Volumesize < newRds.Spec.Volumesize {
 		c.logger.Debug("rdsUpdate: enlarge volume")
 		eventMsg := fmt.Sprint("This instance is enlarging to ", newRds.Spec.Volumesize)
