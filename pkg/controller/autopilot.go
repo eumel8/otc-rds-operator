f4ee3e126e85f394fe74b29ced8141176d87343e
Frank Kloeker <eumel@arcor.de>
2022-05-04 09:15:52 +0200

    start work autopilot feat

---
+++ pkg/controller/autopilot.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SmnReceiver() error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			// _, _ = fmt.Fprint(w, ProviderGetResponse)
+		case "POST":
+			w.Header().Add("X-Subject-Token", "dG9rZW46IDEyMzQK")
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			// _, _ = fmt.Fprint(w, ProviderPostResponse)
+		}
+	})
+	mux.HandleFunc("/notify", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			// _, _ = fmt.Fprint(w, ProviderGetResponse)
+		case "POST":
+			w.Header().Add("X-Subject-Token", "dG9rZW46IDEyMzQK")
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			// _, _ = fmt.Fprint(w, ProviderPostResponse)
+		}
+	})
+	fmt.Println("Listening...")
+
+	var retries int = 3
+
+	for retries > 0 {
+		err := http.ListenAndServe("0.0.0.0:5000", mux)
+		if err != nil {
+			fmt.Println("Restart http server ... ", err)
+			retries -= 1
+		} else {
+			break
+		}
+	}
+	return nil
+}
