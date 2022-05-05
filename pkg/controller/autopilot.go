5af201e338862f8b89c03bd531e59d7e2b73e410
Frank Kloeker <eumel@arcor.de>
2022-05-05 08:13:06 +0200

    rule out subscriber url

--- pkg/controller/autopilot.go
+++ pkg/controller/autopilot.go
@@ -1,12 +1,27 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type Subscriber struct {
+	Subscribeurl     string `json:"subscribe_url"`
+	Signature        string `json:"signature"`
+	Topicurn         string `json:"topic_urn"`
+	Messageid        string `json:"message_id"`
+	Signatureversion string `json:"signature_version"`
+	Type             string `json:"type"`
+	Message          string `json:"message"`
+	Signaturecerturl string `json:"signing_cert_url"`
+	Timestamp        string `json:"timestamp"`
+}
+
 func SmnReceiver() error {
+	var subscriber Subscriber
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +32,17 @@ func SmnReceiver() error {
 				fmt.Println(err)
 			}
 			fmt.Printf("server: request body: %s\n", req)
-
+			err = json.Unmarshal([]byte(req), &subscriber)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if subscriber.Subscribeurl != "" {
+				fmt.Println(subscriber.Subscribeurl)
+				_, err = http.Get(subscriber.Subscribeurl)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
 			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			// _, _ = fmt.Fprint(w, ProviderGetResponse)
