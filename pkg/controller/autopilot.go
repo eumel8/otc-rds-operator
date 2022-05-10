4eec68717f0fecdfdcdf4ec679031c51040646c6
Frank Kloeker <eumel@arcor.de>
2022-05-10 18:58:56 +0200

    debug: dump subscriber message for tags

--- pkg/controller/autopilot.go
+++ pkg/controller/autopilot.go
@@ -103,6 +103,7 @@ func (c *Controller) SmnReceiver() error {
 				c.logger.Info("Event request: ", subscriber.Topicurn)
 				//c.logger.Info("Event message: ", strings.Split(subscriber.Message, ","))
 				rdsName := strings.Split(subscriber.Topicurn, ":")[4]
+				println("SUB :", subscriber.Message)
 				if strings.Contains(subscriber.Message, "rds039_disk_util") {
 					fmt.Println("rds039_disk_util alarm ", rdsName)
 				}
