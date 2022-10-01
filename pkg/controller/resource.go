5d20128060c830712b39b5661c99ad584084801a
Frank Kloeker <f.kloeker@telekom.de>
2022-10-02 00:50:37 +0200

    debug: log job

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -579,7 +579,8 @@ func (c *Controller) rdsUpdate(ctx context.Context, client *golangsdk.ServiceCli
 		}
 
 		if err != nil {
-			time.Sleep(5 * time.Second)
+			c.logger.Debug("DEBUG: sleep for :", newRds)
+			time.Sleep(10 * time.Second)
 			logjob, err := c.kubeClientSet.BatchV1().Jobs(newRds.Namespace).Get(ctx, logInstance, metav1.GetOptions{})
 			err = fmt.Errorf("error getting logfetch job for watch: %v", err)
 			if err != nil {
