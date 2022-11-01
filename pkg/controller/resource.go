dc5e190f4aa43cf9ba629059f0b50c1e17833e5b
Frank Kloeker <f.kloeker@telekom.de>
2022-11-01 04:22:53 +0100

    remove non used func

--- pkg/controller/resource.go
+++ pkg/controller/resource.go
@@ -1017,17 +1017,3 @@ func (c *Controller) UpdateStatus(ctx context.Context, newRds *rdsv1alpha1.Rds) 
 	}
 	return nil
 }
-
-/*
-func Unique(stringSlice []string) []string {
-	keys := make(map[string]bool)
-	list := []string{}
-	for _, entry := range stringSlice {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
-		}
-	}
-	return list
-}
-*/
